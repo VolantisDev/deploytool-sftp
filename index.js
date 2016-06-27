fc076a5f0562318d4cacace56826cee3e82cc4fd
Ben McClure <ben.mcclure@gmail.com>
2016-06-27 01:14:05 -0500

    New plugin architecture for deploytool 0.2.0, version bump

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  */
 
 module.exports = {
+  name: 'sftp',
+  tag: 'deployment',
+  init: function () {},
   deploy: require('./lib/deploy'),
   ftpWrapper: require('./lib/sftp-wrapper'),
   mkdirParent: require('./lib/mkdir-parent')
